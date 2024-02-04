061b90b55422aa771b3097cca01896c30d0266fa
braylon <braylon_hbw@163.com>
2024-02-04 20:57:15 +0800

    add gitignore

---
+++ src/class00/PrintTable.java
@@ -0,0 +1,2 @@
+package class00;public class PrintTable {
+}
--- src/class30/Code01_MorrisTraversal.java
+++ src/class30/Code01_MorrisTraversal.java
@@ -223,8 +223,137 @@ public class Code01_MorrisTraversal {
 		morrisIn(head);
 		morrisPre(head);
 		morrisPos(head);
+		morrisPos_test(head);
+		morris_post(head);
 		printTree(head);
 
 	}
 
+	/**
+	 * morris后序遍历：第二次回到节点的时候，逆序打印左树右边界
+	 * 最后单独打印整棵树的右边界
+	 *
+	 * 只到自己一次的节点不做任何动作
+	 * @param head
+	 */
+	public static void morris_post(Node head){
+		if(head == null){
+			return;
+		}
+		Node cur = head;
+		Node mostRight = null;
+		while (cur != null){
+			if(cur.left == null){
+				cur = cur.right;
+				continue;
+			}
+			mostRight = cur.left;
+			while (mostRight.right != null && mostRight.right != cur){
+				mostRight = mostRight.right;
+			}
+			// 第一次到节点
+			if(mostRight.right == null){
+				mostRight.right = cur;
+				cur = cur.left;
+			} else {
+				// 第二次到节点
+				mostRight.right = null;
+				// 要先恢复左树最右子节点的结构再打印，否则会把整棵树的右边界也给串进来打印
+				// 因为未恢复之前mostRight.right会指向cur节点
+				printLeftTreeRightEdge(cur.left);
+				cur = cur.right;
+			}
+		}
+		printLeftTreeRightEdge(head);
+		System.out.println();
+	}
+
+	private static void printLeftTreeRightEdge(Node head) {
+		Node tail = revese(head);
+		Node cur = tail;
+		while (cur != null){
+			System.out.print(cur.value + " ");
+			cur = cur.right;
+		}
+		revese(tail);
+	}
+
+	private static Node revese(Node head){
+		if(head == null) {
+			return null;
+		}
+		Node pre = null;
+		Node next = null;
+		while (head != null){
+			next = head.right;
+			head.right = pre;
+			pre = head;
+			head = next;
+		}
+		return pre;
+	}
+	public static void morrisPos_test(Node head) {
+		if (head == null) {
+			return;
+		}
+		Node cur = head;
+		Node mostRight = null;
+		while (cur != null) {
+			if(cur.left == null){
+				cur = cur.right;
+				continue;
+			}
+			mostRight = cur.left;
+			while (mostRight.right != null && mostRight.right != cur) {
+				mostRight = mostRight.right;
+			}
+			if (mostRight.right == null) {
+				mostRight.right = cur;
+				cur = cur.left;
+			} else {
+				mostRight.right = null;
+				printEdge(cur.left);
+				cur = cur.right;
+			}
+		}
+		printEdge(head);
+		System.out.println();
+	}
+
+	/**
+	 * 是否是搜索二叉树
+	 * 中序遍历是否升序
+	 * @param head
+	 * @return
+	 */
+	public static boolean is_BST(Node head) {
+		if(head==null){
+			return true;
+		}
+		Node cur = head;
+		Node mostRight = null;
+		boolean ans = true;
+		Integer pre = null;
+		while (cur != null){
+			if(cur.left != null){
+				mostRight = cur.left;
+				while (mostRight.right != null && mostRight.right != cur){
+					mostRight = mostRight.right;
+				}
+				if(mostRight.right == null){
+					mostRight.right = cur;
+					cur = cur.left;
+					continue;
+				}else {
+					mostRight.right = null;
+				}
+			}
+			if(pre != null && pre > cur.value){
+				ans = false;
+			}
+			pre = cur.value;
+			cur = cur.right;
+		}
+		return ans;
+	}
 }
